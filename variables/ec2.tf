76c4a3b5f52ef81d9c015818156f1bb0b7e92f11
madan <madanmohan.devops@gmail.com>
2024-04-05 12:45:58 +0530

    terraform ec2 variables

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.ami_type
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id]
   
   tags = var.tags
 }
--- variables/variables.tf
+++ variables/variables.tf
@@ -21,3 +21,23 @@ variable "tags"{
      }
 
 }
+
+variable "sg-name" {
+  type = string
+  default = "roboshop-all"
+}
+
+variable "sg-description" {
+  type = string
+  default = "allowing all ports"
+}
+
+variable "inbound-from-port" {
+  type = number
+  default = 0
+}
+
+variable "cidr_blocks" {
+    type = list  
+    default = ["0.0.0.0/0"]
+}
