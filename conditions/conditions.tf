ce72e669f1e77451d495a03b31afaec41b183810
madan <madanmohan.devops@gmail.com>
2024-04-28 05:38:42 +0530

    deleted old files

--- conditions/conditions.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_name == "MongoDB" ? "t3.small" : "t2.small"
-  
-  tags = {
-    name = "hello world"
-  }
-}
